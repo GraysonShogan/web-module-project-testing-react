cb2c3ce8861c4d36f47c3b6d33704f340b38c747
Grayson Shogan <graysontoddshogan@gmail.com>
2023-05-10 15:28:44 -0700

    wip

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -13,8 +13,22 @@ test("renders Loading component when prop show is null", () => {
   expect(loading).toBeInTheDocument();
 });
 
-test("renders same number of options seasons are passed in", () => {});
+test("renders same number of options seasons are passed in", () => {
+  render(<Show show={mockData} selectedSeason={"none"} />);
+  const seasonOptions = screen.queryAllByTestId("season-option");
+  expect(seasonOptions).toHaveLength(2);
+});
 
 test("handleSelect is called when an season is selected", () => {});
 
 test("component renders when no seasons are selected and when rerenders with a season passed in", () => {});
+
+const mockData = {
+  name: "noodle",
+  summary: "noodle summary",
+  seasons: [
+    { name: "noodle season 1", id: "1234", episodes: null },
+    { name: "noodle season 2", id: "1235", episodes: null },
+    { name: "noodle season 3", id: "1235", episodes: null },
+  ],
+};
